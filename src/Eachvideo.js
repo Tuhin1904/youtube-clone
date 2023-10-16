5fae2e980a5a3e4be6840a7352e52996900a0172
Tuhin Ghosh <iamtuhinn@gmail.com>
2023-10-16 13:41:55 +0530

    thumbnail and title fetched

--- src/Eachvideo.js
+++ src/Eachvideo.js
@@ -4,12 +4,13 @@ import "./styles/Eachvideo.css"
 function Eachvideo({thumbnail, title, image, views,chanelname, time}) {
   return (
     <div className="eachVid">
-        <img src="" alt="" />
+        <img src={thumbnail.url} alt="" />
         <div className="video_info">
+            <h4>{chanelname}</h4>
             <img src="" alt="" className="thumbnailImg" />
         </div>
         <div className="videoText">
-            <h4>{title}</h4>
+            <h5>{title}</h5>
             <p>{chanelname}</p>
             <p>{views}.{time}</p>
         </div>
--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./styles/RecommendedVideo.css"
 import Eachvideo from './Eachvideo';
+import axios from 'axios';
+
 
 function RecommendedVideos() {
+  const API_key="AIzaSyC5_FfKdlUpBVYKjqzpnAlUKSDPKloF6s8"
+  const youtubeurl="https://www.googleapis.com/youtube/v3/videos?"
+
+  const[apiData,setApiData]=useState([])
+ 
+  useEffect(()=>{
+   
+    axios.get(youtubeurl+ new URLSearchParams({
+      key: API_key,
+      part:'snippet',
+      chart:'mostPopular',
+      maxResult:10,
+      regionCode:"IN"
+    }))
+    .then((res)=>{
+      console.log(res.data.items)
+      setApiData(res.data.items)})
+    .catch(err=> console.log('error is',err))
+  },[])
   return (
     <div className="recVideos">
         <h3>Recommended</h3>
+        <br />
         <div className="eachVideo">
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
-            <Eachvideo title="First vid" views="1M" time="2days ago"/>
+       { apiData.map((item,i)=>{
+
+        return  <Eachvideo key={i} chanelname={item.snippet.channelTitle} title={item.snippet.title} thumbnail={item.snippet.thumbnails.medium} time={item.snippet.publishedAt}/>
+          })}
         </div>
     </div>
   )
